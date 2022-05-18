7c26be1e326b6ab773e791adabf639c84f59ce55
kingjiyun <88700231+kingjiyun@users.noreply.github.com>
2022-05-18 22:59:01 +0900

    [연산자]산술,관계,논리,증감
    주석 처리 돼 있음 증감은 따로있음(안씀)

---
+++ test0515.java
@@ -0,0 +1,42 @@
+public class test0515{
+	public static void main(String [] args){
+		int a =10;
+
+		//산술 연산자
+		//System.out.println((a=20)==20);		//true
+		//System.out.println(!a);			//오류
+		//System.out.println(a);			//20
+		
+		//관계 연산자
+		//boolean power = false;			//false
+		//System.out.println(power);		//true
+		//System.out.println((!!!power));		//true
+		//System.out.println(!power);		//20
+		//System.out.println(a);			//false
+		//System.out.println(false);			//false
+
+		//논리 연산자
+		//System.out.println((1>2)&(1<(a=20)));	//false
+		//System.out.println((1>2)&&(1<(a=20)));	//false
+		//System.out.println((1<2) | (1>2));		//true
+		//System.out.println((1<2) ||(1>2));		//true
+		//System.out.println(!(1<2));		//false
+
+		//증감 연산자
+		//int b=++a+10;
+		//System.out.println(a);			//11
+		//System.out.println(b);			//21
+
+		//int b= a++ +10;
+		//System.out.println(a);			//11
+		//System.out.println(b);			//20
+
+		//int b= --a+ 10;
+		//System.out.println(a);			//9	
+		//System.out.println(b);			//19
+
+		//int b= a-- +10;
+		//System.out.println(a);			//9
+		//System.out.println(b);			//20
+	}
+}
