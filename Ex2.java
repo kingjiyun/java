e0135d3c543fafb8d31f4f0860d7c4cf06b4f958
kingjiyun <88700231+kingjiyun@users.noreply.github.com>
2022-05-18 22:02:08 +0900

    [출력문]자기소개

---
+++ Ex2.java
@@ -0,0 +1,9 @@
+public class Ex2 {
+     public static void main ( String args[]) {
+            System.out.println ("강지윤입니다.");
+            System.out.println ("강아지를 좋아합니다.");
+            System.out.println ("딸기도 좋아합니다.");
+            System.out.println ("빨리 밥을 먹습니다.");
+              System.out.println ("빨리 밥을 먹습니다.");
+          }
+}
