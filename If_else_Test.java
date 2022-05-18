e3897af66e2eb280f768dd8eb065bf61e1565f69
kingjiyun <88700231+kingjiyun@users.noreply.github.com>
2022-05-18 22:52:24 +0900

    [if]if랑else
    3의 배수

---
+++ If_else_Test.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class If_else_Test{
+	public static void main(String []args){
+		Scanner sc= new Scanner(System.in);
+		System.out.print("수를 입력하시오:");
+		int number = sc.nextInt();
+
+		if(number %3==0)
+		System.out.println("3의 배수입니다.");
+
+		else
+		System.out.println("3의 배수가 아닙니다.");
+  	}
+}
