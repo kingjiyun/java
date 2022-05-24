95ca53922f7acdd3879bccd7d31c420f337a6305
kingjiyun <88700231+kingjiyun@users.noreply.github.com>
2022-05-24 22:16:55 +0900

    [if else if else]급식순서
    급식순서 구하기

---
+++ If_L.java
@@ -0,0 +1,17 @@
+public class If_L{
+	public static void main{
+		int num
+		if(num==1){
+		System.out.println("급식순서 첫번째");
+		}
+		if else (num==2){
+		System.out.println("급식순서 두번째");
+		}
+		if else (num==3){
+		System.out.println("급식순서 세번째.");
+		}
+		 else {
+		System.out.println("잘못입력.");
+		}
+	}
+}
